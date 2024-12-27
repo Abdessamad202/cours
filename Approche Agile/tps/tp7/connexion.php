d60d7da39fb3d57991cbe4715357b70bfce6794c
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-27 23:52:00 +0100

    Updated connexion.php

--- Approche Agile/tps/tp7/connexion.php
+++ Approche Agile/tps/tp7/connexion.php
@@ -5,7 +5,7 @@ $password = "1234";
 $database = "todolist";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
-
+$task = new App\Task($conn);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
