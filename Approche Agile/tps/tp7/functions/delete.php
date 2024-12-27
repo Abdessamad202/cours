2e32c74d306de944ce275967187cb617214b52b1
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-27 23:52:03 +0100

    Updated delete.php in functions

--- Approche Agile/tps/tp7/functions/delete.php
+++ Approche Agile/tps/tp7/functions/delete.php
@@ -1,5 +1,4 @@
 <?php
 if (isset($_POST['delete'])) {
-  $deletTaskQuery = $conn->prepare('DELETE FROM todo WHERE id=?');
-  $deletTaskQuery->execute([$_POST['task_id']]);
+  $task->delete($_POST['task_id']);
 }
