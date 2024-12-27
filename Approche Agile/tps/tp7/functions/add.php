56856ec054b300c5d8432e9826b9a8fcd48a4067
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-27 23:52:02 +0100

    Updated add.php in functions

--- Approche Agile/tps/tp7/functions/add.php
+++ Approche Agile/tps/tp7/functions/add.php
@@ -3,8 +3,7 @@ if (isset($_POST["new"])) {
   if(trim($_POST['title']) === ''){
     $error = 'the title should not be empty';
   }else{
-    $insertTaskQuery = $conn->prepare("INSERT INTO todo (title) VALUES (?);");
-    $insertTaskQuery->execute([$_POST["title"]]);
+    $task->add($_POST['title']);
   }
   // echo "good";
   # code...
